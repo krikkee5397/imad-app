2f8b5fb8669c78f0e8865f434be3b3d3e452549b
ricky <30741766+krikkee5397@users.noreply.github.com>
2017-08-06 08:57:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.get('/', function (req, res) {
 app.get('/article-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('/article-one',function(req,res){
-    res.send('hehe hello brother');
+app.get('/article-two',function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three',function(req,res){
-    res.send('haha hello brother');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
