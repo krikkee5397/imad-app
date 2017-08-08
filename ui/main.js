5337e4eaa93e852f3ecaa33bc312a99deaedd464
ricky <30741766+krikkee5397@users.noreply.github.com>
2017-08-08 19:48:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+var img=document.getElementByid('madi');
+var marginleft=0;
+function moveright() {
+    marginleft=marginleft + 5;
+    img.style.marginleft=marginleft +'px';
+}
+img.onclick=function() {
+    var interval =setInterval(moveright,50);
+};
