21025bbb615db146c88c335637278228ab706d9b
ricky <30741766+krikkee5397@users.noreply.github.com>
2017-08-09 01:05:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,10 @@ var element=document.getElementById('main');
 element.innerHTML='ricky';
 var img=document.getElementById('madi');
 var marginleft=0;
-function moveright() {
-    marginleft=marginleft + 1;
-    img.style.marginleft=marginleft +'px';
+function moveRight() {
+    marginLeft=marginLeft + 1;
+    img.style.marginLeft=marginLeft +'px';
 }
 img.onclick=function() {
-    var interval =setInterval(moveright,10);
+    var interval =setInterval(moveRight,10);
 };
