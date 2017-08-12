d634949ee9acf0de18cb93018d08af662d02649e
ricky <30741766+krikkee5397@users.noreply.github.com>
2017-08-13 00:25:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,8 +111,8 @@ submit.onclick=function(){
     
  var names=['name1','name2','name3']; 
  var list='';
- for(var i=0;i<name.length;i++){
-list +='<li>'+name[i]+'</li>';
+ for(var i=0;i<names.length;i++){
+list +='<li>'+names[i]+'</li>';
 }
 var ul=document.getElementById('namelist');
 ul.innerHTML=list;
