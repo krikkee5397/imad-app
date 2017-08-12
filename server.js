fc5351228797576ba124283834f182fcd543de72
ricky <30741766+krikkee5397@users.noreply.github.com>
2017-08-13 00:08:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,3 +104,16 @@ var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+var submit=document.getElementById("submiit_btn");
+submit.onclick=function(){
+    
+ var names=['name1','name2','name3']; 
+ var list='';
+ for(var i=0;i<name.length;i++){
+list +='<li>'+name[i]+'</li>';
+}
+var ul=document.getElementById('namelist');
+innerHTML=list;
+};
