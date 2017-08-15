2e823fe44ee0cfa451639df8fd1ef05a74cd9f6a
ricky <30741766+krikkee5397@users.noreply.github.com>
2017-08-15 13:55:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-var element=document.getElementById('main');
+/*var element=document.getElementById('main');
 element.innerHTML='ricky';
 var img=document.getElementById('madi');
 var marginLeft=0;
@@ -29,7 +29,7 @@ button.onclick=function(){
    request.open('GET','http://krikkee5397.imad.hasura.app-io/counter',true);
    request.send(null);
 };
-var submit=getElementById('submit_btn');
+var submit=getElementById('submit_btn');*/
 submit.onclick=function(){
    var request=new XMLHttpRequest();
   request.onreadystatechange=function(){
