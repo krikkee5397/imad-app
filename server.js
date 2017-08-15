0bf9fcfabf13431c9852ee7756e70a34effc118e
ricky <30741766+krikkee5397@users.noreply.github.com>
2017-08-15 09:35:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 var names=[];
 app.get('/submit-name/:name', function (req, res){
-    var name=re.params.name;
+    var name=req.params.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
