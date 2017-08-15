bf14f7e00cfad40afe89cfe4055b5bfa678cb317
ricky <30741766+krikkee5397@users.noreply.github.com>
2017-08-15 09:00:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,16 @@ img.onclick=function() {
 var Counter=0;
 var button=document.getElementById('Counter');
 button.onclick=function(){
+  var request=new XMLHttpRequest();
+  request.onreadystatechange=function(){
+      if(request.readystate==XMLHttpRequest.DONE)
+      {
+          if(request.status==200)
+          {
+              var counter=request.responseText;
+          }
+      }
+  }
   Counter=Counter+1;
   var span=document.getElementById('count');
   span.innerHTML=Counter.toString();
