ed4080fe9d581464caf83576ace0908cf136174e
ricky <30741766+krikkee5397@users.noreply.github.com>
2017-08-19 15:20:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,24 +11,27 @@ img.onclick=function() {
     var interval =setInterval(moveRight,10);
 };*/
 var button=document.getElementById('counter');
+var counter=0;
 button.onclick=function(){
-  var request=new XMLHttpRequest();
+    counter=counter+1;
+    
+/*  var request=new XMLHttpRequest();
   request.onreadystatechange=function(){
       if(request.readystate===XMLHttpRequest.DONE)
       {
           if(request.status===200)
           {
-              var counter=request.responseText;
+              var counter=request.responseText;*/
               var span=document.getElementById('count');
               span.innerHTML=counter.toString();
               
-          }
-      }
+       //   }
+      //}
   };
   
-   request.open('GET','http://krikkee5397.imad.hasura.app-io/counter',true);
+   /*request.open('GET','http://krikkee5397.imad.hasura.app-io/counter',true);
    request.send(null);
-};
+};*/
 /*var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
    var request=new XMLHttpRequest();
