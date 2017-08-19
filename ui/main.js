4174c06b46564ec59d1ff16fab889b6149cd5f7a
ricky <30741766+krikkee5397@users.noreply.github.com>
2017-08-19 15:36:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,8 @@ button.onclick=function(){
   request.onreadystatechange=function(){
       if(request.readystate===XMLHttpRequest.DONE)
       {
-          if(request.status===200)
+         // if(request.status===200)
+         if (this.readyState == 4 && this.status == 200)
           {
               var counter=request.responseText;
               var span=document.getElementById('count');
